eca4c67fdbdc6aba9cb551eb2bf88e6a9c21b30d
tob1tbs <chikhladze.mt@gmail.com>
2026-01-29 23:57:54 +0400

    Update delivery-info.blade.php

--- resources/views/livewire/web/product/delivery-info.blade.php
+++ resources/views/livewire/web/product/delivery-info.blade.php
@@ -4,14 +4,9 @@
 <table class="table table-borderless fs-sm mb-2">
     <tbody>
     <tr>
-        <td class="py-2 ps-0">თბილისი</td>
-        <td class="py-2 text-center">მიწოდება მომდევნო დღეს</td>
-        <td class="text-body-emphasis fw-semibold text-end py-2 pe-0">უფასო</td>
+        <td class="py-2 ps-0">მიწოდება</td>
+        <td class="py-2 text-center">მთელი ქვეყნის მასშტაბით</td>
     </tr>
     <tr>
-        <td class="py-2 ps-0">რეგიონი</td>
-        <td class="py-2 text-center">მიწოდება 3-5 სამუშაო დღეში</td>
-        <td class="text-body-emphasis fw-semibold text-end py-2 pe-0">უფასო</td>
-    </tr>
     </tbody>
 </table>
