7f538b710f5d8e7ecbe8fe259174edafa1dee545
tob1tbs <chikhladze.mt@gmail.com>
2026-02-01 01:21:07 +0400

    Update BOGPayment.php

--- app/Services/Payments/BOGPayment.php
+++ app/Services/Payments/BOGPayment.php
@@ -41,7 +41,7 @@ class BOGPayment
             ];
         }
         $payload = [
-            'callback_url' => 'https://example.com/callback',
+            'callback_url' => 'https://iapi.ge/bog/api/payment/callback',
             'external_order_id' => $order->id,
             'purchase_units' => [
                 'currency' => 'GEL',
