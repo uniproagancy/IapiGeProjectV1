ca4e334984f2196a83755620de6a7a6d1f17cfb5
uniproagancy <developer@unipro.ge>
2026-02-01 05:52:00 +0400

    Update credo.blade.php

--- resources/views/credo.blade.php
+++ resources/views/credo.blade.php
@@ -1,7 +1,19 @@
-<form action="https://ganvadeba.credo.ge/widget/" method="post" id="credoForm">
-    <input type="hidden" name="credoinstallment" value='{{ $data }}' />
+<form action="http://ganvadeba.credo.ge/widget/" method="post">
+    <input type="hidden" name="credoinstallment"
+           value='{"merchantId":"12545",
+"orderCode":"17407",
+"check":"f61136837ebe753b4a1e9b9f9893f805",
+"products":[
+{"id":"4634",
+"title":"PHILIPS HP6549/00",
+"amount":"2",
+"price":"21400",
+"type":"0"}
+ ]
+}' />
     <input type="submit" value="go" />
 </form>
+
 
 
 
