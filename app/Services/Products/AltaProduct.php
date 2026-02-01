eea946f422405521c587fbaa496bff2d4ce3d2d6
uniproagancy <developer@unipro.ge>
2026-02-01 19:22:40 +0400

    Update AltaProduct.php

--- app/Services/Products/AltaProduct.php
+++ app/Services/Products/AltaProduct.php
@@ -95,7 +95,7 @@ class AltaProduct
 
                         $body = $response->getBody()->getContents();
                         $data = json_decode($body, true);
-                        Log::info($data);
+                        Log::info($data['product']);
                         if (!isset($data['product']) ||
                             $data['product'] === null ||
                             !($data['product']['isInStock'] ?? false)) {
