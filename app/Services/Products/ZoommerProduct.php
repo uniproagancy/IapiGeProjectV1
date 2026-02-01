b942e5bb64e514e74054094903ef6da08f7f77fa
uniproagancy <developer@unipro.ge>
2026-02-01 17:36:03 +0400

    Update ZoommerProduct.php

--- app/Services/Products/ZoommerProduct.php
+++ app/Services/Products/ZoommerProduct.php
@@ -15,8 +15,8 @@ class ZoommerProduct
     protected int $concurrent_requests = 20; // შემცირდა 50-დან 20-მდე
     protected int $chunk_size = 100; // შემცირდა 500-დან 100-მდე
     protected int $timeout = 30;
-    protected int $start_id = 1;
-    protected int $end_id = 100000;
+    protected int $start_id = 161581;
+    protected int $end_id = 161583;
 
     public function scanAllIds(): array
     {
