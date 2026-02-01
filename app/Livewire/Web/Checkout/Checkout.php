50a1334a71b74eb2a307add86057a209bcad0bd6
uniproagancy <developer@unipro.ge>
2026-02-01 11:42:56 +0400

    Update Checkout.php

--- app/Livewire/Web/Checkout/Checkout.php
+++ app/Livewire/Web/Checkout/Checkout.php
@@ -347,6 +347,7 @@ class Checkout extends Component
                         ];
                     }
                     Log::info($products);
+                    Log::info($order->amount + ($order->amount + 0.1));
                     $tbcInstallment->addProducts($products);
                     $response = $tbcInstallment->applyInstallmentApplication($order->id, $order->amount + ($order->amount + 0.1));
                     Log::info($response);
