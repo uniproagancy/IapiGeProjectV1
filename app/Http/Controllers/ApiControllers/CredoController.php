cfd277b662bab0d418b1cc239e8c443b7c16daa3
uniproagancy <developer@unipro.ge>
2026-02-01 05:45:45 +0400

    Update CredoController.php

--- app/Http/Controllers/ApiControllers/CredoController.php
+++ app/Http/Controllers/ApiControllers/CredoController.php
@@ -3,23 +3,26 @@
 namespace App\Http\Controllers\ApiControllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Order\Order;
 use Illuminate\Http\Request;
 
 class CredoController extends Controller
 {
     //
-    public function createOrder()
+    public function createOrder(Request $request)
     {
-        $products = [
-            [
-                'id' => '4634',
+        $order = Order::find($request->order_id);
+        $products = [];
+        foreach ($order->items as $item) {
+            $products[] = [
+                'id' => $item->id,
                 'title' => 'PHILIPS HP6549/00',
-                'amount' => '2',
-                'price' => '21400', // თეთრებში
+                'amount' => $item->quantity,
+                'price' => $item->price + ($item->price * 0.1), // თეთრებში
                 'type' => '0',
-            ]
-        ];
-        $checkString = '';
+            ];
+        }
+        $checkString = '12545';
         foreach ($products as $p) {
             $checkString .= $p['id'] . $p['title'] . $p['amount'] . $p['price'] . $p['type'];
         }
