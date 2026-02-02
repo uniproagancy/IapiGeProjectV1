1341cfb8997fbee3f33a7dd9b2422528186f98eb
tob1tbs <chikhladze.mt@gmail.com>
2026-02-02 16:42:59 +0400

    Update search-offcanvas.blade.php

--- resources/views/livewire/web/search/search-offcanvas.blade.php
+++ resources/views/livewire/web/search/search-offcanvas.blade.php
@@ -3,10 +3,10 @@
          id="searchOffcanvas"
          tabindex="-1"
          aria-labelledby="searchOffcanvas"
-         style="width: 500px; z-index: 1060; background-color: rgba(0,0,0,0.1)">
+         style="width: 500px; z-index: 1060; background-color: rgba(0,0,0,0.05)">
         <div class="offcanvas-header flex-column align-items-start py-3 pt-lg-4">
             <div class="d-flex align-items-center justify-content-between w-100 mb-3 mb-lg-4">
-                <button type="button" class="btn-close text-white color-white" data-bs-dismiss="offcanvas" aria-label="დახურვა"></button>
+                <button type="button" class="btn-close text-white color-white" data-bs-dismiss="offcanvas" aria-label="დახურვა" style="background-color: #252525"></button>
             </div>
         </div>
         <div class="offcanvas-body d-flex flex-column gap-4 pt-2">
