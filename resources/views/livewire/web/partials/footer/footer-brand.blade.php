6714e3ff74d6f43deecd369b86bc21bc73348682
tob1tbs <chikhladze.mt@gmail.com>
2026-02-04 02:00:38 +0400

    SOC

--- resources/views/livewire/web/partials/footer/footer-brand.blade.php
+++ resources/views/livewire/web/partials/footer/footer-brand.blade.php
@@ -29,4 +29,15 @@
         <p class="text-body fs-sm text-sm-end text-md-start mb-sm-0 mb-md-3 ms-0 ms-sm-auto ms-md-0 me-4 text-uppercase">
             +995 555 700 720</p>
     </div>
+    <div class="d-flex gap-3 mt-1">
+        <a class="btn btn-icon btn-secondary rounded-circle" href="https://www.instagram.com/iapi_ge/" aria-label="Instagram">
+            <i class="ci-instagram fs-base"></i>
+        </a>
+        <a class="btn btn-icon btn-secondary rounded-circle" href="https://www.facebook.com/profile.php?id=100091503274684" aria-label="Facebook">
+            <i class="ci-facebook fs-base"></i>
+        </a>
+        <a class="btn btn-icon btn-secondary rounded-circle" href="https://www.tiktok.com/@iapi.ge" aria-label="TikTok">
+            <i class="ci-tiktok fs-base"></i>
+        </a>
+    </div>
 </div>
