96264f99f3db219e236582e89bd73d7b91cd5e64
tob1tbs <chikhladze.mt@gmail.com>
2026-02-04 17:25:56 +0400

    UPDT

--- resources/views/livewire/web/partials/category-offcanvas.blade.php
+++ resources/views/livewire/web/partials/category-offcanvas.blade.php
@@ -11,9 +11,25 @@
         </div>
         <div class="offcanvas-body d-flex flex-column gap-4 pt-2 p-0">
             <ul class="w-100 rounded-top-0 rounded-bottom-4 py-1" style="list-style: none; padding: 0 !important;">
-                @foreach($product_categories as $category)
-                    @include('livewire.web.partials.header.category-item', ['category' => $category])
-                @endforeach
+                <li class="position-static border-bottom category-item">
+                    <div class="position-relative rounded pt-2 pb-2 px-4">
+                        <a class="dropdown-item fw-medium stretched-link d-none d-lg-flex font-neue"
+                           href="{{ route('web.products.index', $category->translation(app()->getLocale())->slug ?? $category->translation('ka')->slug) }}">
+                            <img src="{{ asset('web-assets/icons/categories/'.$category->id.'.svg') }}" width="25">
+                            <span class="text-truncate" style="font-size: 13px; padding-left: 5px">
+                                {{ $category->translation(app()->getLocale())->title ?? $category->translation('ka')->title }}
+                            </span>
+                            @if($category->children->where('active', 1)->where('show', 1)->count() > 0)
+                                <i class="ci-chevron-right fs-base ms-auto me-n1"></i>
+                            @endif
+                        </a>
+                        <a href="{{ route('web.products.index', $category->translation(app()->getLocale())->slug ?? $category->translation('ka')->slug) }}"
+                           class="fw-medium text-wrap stretched-link d-lg-none font-neue category-link" style="font-size: 13px">
+                            <img src="{{ asset('web-assets/icons/categories/'.$category->id.'.svg') }}" width="25">
+                            {{ $category->translation(app()->getLocale())->title ?? $category->translation('ka')->title }}
+                        </a>
+                    </div>
+                </li>
             </ul>
         </div>
     </div>
@@ -24,5 +40,11 @@
         .offcanvas-backdrop {
             transition: opacity 0.3s ease;
         }
+    </style>
+    <style>
+        .category-item:hover a {
+            color: #ff6900;
+        }
+        .category-link { color: #252525; text-decoration: none}
     </style>
 </div>
--- resources/views/livewire/web/partials/header/category-item.blade.php
+++ resources/views/livewire/web/partials/header/category-item.blade.php
@@ -16,30 +16,10 @@
             {{ $category->translation(app()->getLocale())->title ?? $category->translation('ka')->title }}
         </a>
     </div>
-{{--    @if($category->children->where('active', 1)->where('show', 1)->count() > 0)--}}
-{{--        <div class="dropdown-menu rounded-4 p-4"--}}
-{{--             style="top: 1rem; height: calc(100% - .1875rem); --cz-dropdown-spacer: .3125rem; animation: none;">--}}
-{{--            <div class="d-flex flex-column flex-lg-row h-100 gap-4">--}}
-{{--                <div style="min-width: 194px">--}}
-{{--                    <ul class="nav flex-column gap-2 mt-n2">--}}
-{{--                        @foreach($category->children->where('active', 1)->where('show', 1) as $childCategory)--}}
-{{--                            <li class="d-flex w-100 pt-1">--}}
-{{--                                <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0"--}}
-{{--                                   href="{{ route('web.products.index', $childCategory->translation(app()->getLocale())->slug ?? $childCategory->translation('ka')->slug) }}">--}}
-{{--                                    {{ $childCategory->translation(app()->getLocale())->title ?? $childCategory->translation('ka')->title }}--}}
-{{--                                </a>--}}
-{{--                            </li>--}}
-{{--                        @endforeach--}}
-{{--                    </ul>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--        </div>--}}
-{{--    @endif--}}
 </li>
 <style>
     .category-item:hover a {
         color: #ff6900;
     }
-
     .category-link { color: #252525; text-decoration: none}
 </style>
