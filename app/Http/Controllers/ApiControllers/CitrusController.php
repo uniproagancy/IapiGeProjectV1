5f7f1118e7d29f2b274208156bd1e4c9c52db433
tob1tbs <chikhladze.mt@gmail.com>
2026-02-04 11:57:39 +0400

    Update CitrusController.php

--- app/Http/Controllers/ApiControllers/CitrusController.php
+++ app/Http/Controllers/ApiControllers/CitrusController.php
@@ -88,7 +88,7 @@ class CitrusController extends Controller
             $product = Product::create([
                 'supplier_product_id' => $productData['id'],
                 'brand_id' => 115,
-                'category_id' => 23,
+                'category_id' => 39,
                 'sku' => 'GL-'.$productData['id'],
                 'supplier_id' => 5,
                 'main_image' => 1,
