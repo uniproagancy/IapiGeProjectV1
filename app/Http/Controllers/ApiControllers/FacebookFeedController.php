95bba800ca1f65fbce09caa35ff538eccf181fc1
tob1tbs <chikhladze.mt@gmail.com>
2026-02-05 13:17:23 +0400

    Feed

--- app/Http/Controllers/ApiControllers/FacebookFeedController.php
+++ app/Http/Controllers/ApiControllers/FacebookFeedController.php
@@ -4,12 +4,38 @@ namespace App\Http\Controllers\ApiControllers;
 
 use App\Http\Controllers\Controller;
 use App\Models\Product\Product;
-use Illuminate\Support\Facades\Log;
+use MeeeetDev\LaravelFacebookCatalog\LaravelFacebookCatalog;
+
 
 class FacebookFeedController extends Controller
 {
 
     public function getFeed() {
+        LaravelFacebookCatalog::setTitle('Example feed');
+        LaravelFacebookCatalog::setDescription('Example feed of the Example shop');
+        LaravelFacebookCatalog::setLink('https://example.shop');
+        LaravelFacebookCatalog::setCurrency('USD');
+
+        $products = Product::where('active', 1)
+            ->where('in_stock', 1)
+            ->where('show', 1)
+            ->limit(20)
+            ->get();
+
+        foreach($products as $product) {
+            LaravelFacebookCatalog::addItem([
+                'link' => 'https://example.shop/p/foo-bar',
+                'id' => 'SKU123',
+                'title' => 'Foo bar',
+                'image_link' => 'https://example.shop/images/foo-bar.png',
+                'description' => 'Foo bar best product',
+                'availability' => 'in stock',
+                "price" => 99.99,
+                'brand' => 'Foo brand',
+                'condition' => 'new',
+            ]);
+        }
+        return LaravelFacebookCatalog::display();
     }
 
 }
