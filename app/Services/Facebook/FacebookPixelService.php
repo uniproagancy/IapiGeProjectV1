4b7fd6532a4ec4142bfea52c734a1fbba04b958a
tob1tbs <chikhladze.mt@gmail.com>
2026-02-06 16:23:00 +0400

    Update FacebookPixelService.php

--- app/Services/Facebook/FacebookPixelService.php
+++ app/Services/Facebook/FacebookPixelService.php
@@ -20,7 +20,7 @@ class FacebookPixelService
     {
         $this->pixelId = config('services.facebook.pixel_id', '1280014533998229');
         $this->accessToken = config('services.facebook.access_token', 'EAACRpZCqfAR0BQqXVLIuKjIkrDyqOw4KZC68mb5Ov3nHlnUGwQ55YBtDqSqt3ht8g44ClFnK5eNEqT75qeMcuh749JvbfONOqAfjeaLcYZBNzfJhrdRcZAzy7ThPbvjK5p717jLFvWH6Q7KdDWmLVYIJNQw1fbBKVo9eitQtCgONvp1U1R6XhdDtZAKUnNgZDZD');
-        $this->apiVersion = config('services.facebook.api_version', 'v18.0');
+        $this->apiVersion = config('services.facebook.api_version', 'v24.0');
         if (empty($this->pixelId) || empty($this->accessToken)) {
             Log::warning('⚠️  Facebook Pixel credentials not configured');
         }
