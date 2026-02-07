23a60bedb04c359f29346a622348ae153017ab57
tob1tbs <chikhladze.mt@gmail.com>
2026-02-07 15:04:34 +0400

    UPDT

--- app/Http/Controllers/ApiControllers/CredoController.php
+++ app/Http/Controllers/ApiControllers/CredoController.php
@@ -35,6 +35,7 @@ class CredoController extends Controller
                 'products'   => $products,
             ];
             $data = json_encode($payload);
+//            dd($data);
             return view('credo', ['data' => $data]);
         }
     }
