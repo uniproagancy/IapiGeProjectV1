106c0c473fd6b97a174d52271532a0e0bc00767d
tob1tbs <chikhladze.mt@gmail.com>
2026-02-08 20:21:52 +0400

    Update FacebookFeedController.php

--- app/Http/Controllers/FacebookFeedController.php
+++ app/Http/Controllers/FacebookFeedController.php
@@ -21,7 +21,7 @@ class FacebookFeedController extends Controller
         $products = Product::where('active', 1)
             ->where('in_stock', 1)
             ->where('show', 1)
-            ->limit(100)
+//            ->limit(100)
             ->get();
         foreach($products as $product) {
             if($product->main_image != 1) {
