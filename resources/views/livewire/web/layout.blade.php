33f5c4f2a84587adf93f706bc2eff95af215e757
tob1tbs <chikhladze.mt@gmail.com>
2026-02-10 18:50:05 +0400

    Update layout.blade.php

--- resources/views/livewire/web/layout.blade.php
+++ resources/views/livewire/web/layout.blade.php
@@ -202,19 +202,6 @@
 
 @yield('page_scripts')
 <script src="{{ asset('web-assets/js/theme.min.js') }}"></script>
-<!--Start of Tawk.to Script-->
-<script type="text/javascript">
-    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-    (function(){
-        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-        s1.async=true;
-        s1.src='https://embed.tawk.to/686bd1bca86aec190ca6b61d/1iviimin6';
-        s1.charset='UTF-8';
-        s1.setAttribute('crossorigin','*');
-        s0.parentNode.insertBefore(s1,s0);
-    })();
-</script>
-<!--End of Tawk.to Script-->
 <script>
     document.addEventListener('livewire:initialized', () => {
         Livewire.on('ui:error', (data) => {
@@ -292,5 +279,18 @@
         z-index: 10;
     }
 </style>
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+        s1.async=true;
+        s1.src='https://embed.tawk.to/686bd1bca86aec190ca6b61d/1iviimin6';
+        s1.charset='UTF-8';
+        s1.setAttribute('crossorigin','*');
+        s0.parentNode.insertBefore(s1,s0);
+    })();
+</script>
+<!--End of Tawk.to Script-->
 </body>
 </html>
