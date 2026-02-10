c2cfd37a1b0ef93485726f178ae2721d5fc53616
tob1tbs <chikhladze.mt@gmail.com>
2026-02-10 20:02:40 +0400

    Update index.blade.php

--- resources/views/livewire/web/main/index.blade.php
+++ resources/views/livewire/web/main/index.blade.php
@@ -58,9 +58,8 @@
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-
-        fbq('init', '{{ config('services.facebook.pixel_id') }}');
-        fbq('track', 'PageView');
+        fbq('init', '1280014533998229');
+        fbq('track', 'PageView', {}, { eventID: '{{ $event_id }}' });
     </script>
     <noscript><img height="1" width="1" style="display:none"
                    src="https://www.facebook.com/tr?id=1280014533998229&ev=PageView&noscript=1"
