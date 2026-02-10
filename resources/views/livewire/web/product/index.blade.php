ff559355506619c3820f78ff722d1277737a15de
tob1tbs <chikhladze.mt@gmail.com>
2026-02-11 00:00:32 +0400

    Update index.blade.php

--- resources/views/livewire/web/product/index.blade.php
+++ resources/views/livewire/web/product/index.blade.php
@@ -178,6 +178,15 @@
 @section('fb_pixel')
     <script>
         // ✅ Browser-side Custom Event: CategoryView
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '1280014533998229');
         fbq('trackCustom', 'CategoryView', {
             content_name: '{{ $currentCategory?->translation('ka')->title }}',
             content_category: '{{ $currentCategory?->translation('ka')->slug }}',
