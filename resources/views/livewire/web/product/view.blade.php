bd372b7986bea99d579300471576c738da462207
tob1tbs <chikhladze.mt@gmail.com>
2026-02-10 22:35:20 +0400

    Update view.blade.php

--- resources/views/livewire/web/product/view.blade.php
+++ resources/views/livewire/web/product/view.blade.php
@@ -211,7 +211,4 @@
             eventID: '{{ $event_id }}'
         });
     </script>
-    <noscript><img height="1" width="1" style="display:none"
-                   src="https://www.facebook.com/tr?id=1280014533998229&ev=PageView&noscript=1"
-        /></noscript>
 @endsection
