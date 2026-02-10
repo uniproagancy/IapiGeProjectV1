0a9128042f3140d691bf296711e70fbb5e54cfa9
tob1tbs <chikhladze.mt@gmail.com>
2026-02-10 20:41:05 +0400

    Update Index.php

--- app/Livewire/Web/Main/Index.php
+++ app/Livewire/Web/Main/Index.php
@@ -68,7 +68,8 @@ class Index extends Component
     public function render()
     {
         $event_id = 'pv_'.time().'_'.Str::random(6);
-        $this->fbPixel->trackPageViewWithTest();
+        $this->fbPixel->trackPageViewWithTest('TEST68876', [
+        ]);
 
 //        $this->google = new GoogleSheet();
 //        $this->spreadsheetId = '1YvGwk6pilN_S4O9ZYP4-CRxgygPLSjPS';
