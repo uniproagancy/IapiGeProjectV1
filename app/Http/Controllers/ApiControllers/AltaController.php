73033852e4a217b0eb72040e2b372a2507d76c71
tob1tbs <chikhladze.mt@gmail.com>
2026-02-11 20:13:14 +0400

    Update AltaController.php

--- app/Http/Controllers/ApiControllers/AltaController.php
+++ app/Http/Controllers/ApiControllers/AltaController.php
@@ -7,6 +7,7 @@ use App\Models\AltaID;
 use App\Models\Product\Product;
 use App\Services\Products\AltaService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class AltaController extends Controller
 {
@@ -52,6 +53,7 @@ class AltaController extends Controller
         $ids = AltaID::all();
         foreach($ids as $id) {
             Product::where(['sku' => 'ALTA-'.$id['product_id']])->update(['active' => 1, 'show' => 1]);
+            Log::warning('ALTA-'.$id['product_id'].' Updated');
         }
     }
 }
