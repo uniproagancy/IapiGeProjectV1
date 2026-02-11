5302ff1cd3a5631c673bf062e0ecf8dc1d564662
tob1tbs <chikhladze.mt@gmail.com>
2026-02-12 01:11:11 +0400

    Update add-to-cart-button.blade.php

--- resources/views/livewire/web/components/add-to-cart-button.blade.php
+++ resources/views/livewire/web/components/add-to-cart-button.blade.php
@@ -1,7 +1,11 @@
 <div>
     <button type="button"
             wire:click="addProduct"
-            onclick="pixelAddToCart({{ $productId }}, {{ $productPrice }}, {{ $productTitle }})"
+            onclick="pixelAddToCart(
+                {{ $productId }},
+                {{ $productPrice }},
+                @js($productTitle)
+            )"
             class="product-card-button btn btn-icon btn-primary animate-slide-end ms-2"
             aria-label="კალათაში დამატება"
             wire:loading.attr="disabled">
