66bbeac83862f40335b8e7abd0c8ed18079d3c61
tob1tbs <chikhladze.mt@gmail.com>
2026-02-16 23:43:03 +0400

    Update AltaController.php

--- app/Http/Controllers/ApiControllers/AltaController.php
+++ app/Http/Controllers/ApiControllers/AltaController.php
@@ -22,7 +22,7 @@ class AltaController extends Controller
 
     public function scan()
     {
-        return app(AltaService::class)->setIdRange(45000, 50000)->scanAllIds();
+        return app(AltaService::class)->setIdRange(1, 60000)->scanAllIds();
     }
 
     public function altaTrash()
