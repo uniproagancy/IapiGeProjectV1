3c6bd68d9037e3a13aed4256848bdf7d99a2dd22
tob1tbs <chikhladze.mt@gmail.com>
2026-02-16 23:12:57 +0400

    Update AltaController.php

--- app/Http/Controllers/ApiControllers/AltaController.php
+++ app/Http/Controllers/ApiControllers/AltaController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\ApiControllers;
 use App\Http\Controllers\Controller;
 use App\Models\AltaID;
 use App\Models\Product\Product;
+use App\Services\Products\AltaProduct;
 use App\Services\Products\AltaService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
@@ -12,15 +13,48 @@ use Illuminate\Support\Facades\Log;
 class AltaController extends Controller
 {
     //
-    private $altaService;
+    private $priceService;
 
-    public function __construct(AltaService $altaService)
+    public function __construct(AltaService $priceService)
     {
-        $this->altaService = $altaService;
+        $this->priceService = $priceService;
     }
 
     public function scan()
     {
-        return $this->altaService->getAltaB2B();
+        return app(AltaService::class)->scanAllIds();
+    }
+
+    public function altaTrash()
+    {
+        try {
+            $username = 'UNIPRO_GP';
+            $password = 'unipro2020';
+
+            $priceList = $this->priceService->getPriceList(
+                $username,
+                $password,
+            );
+
+            return response()->json([
+                'status' => 'success',
+                'data' => $priceList
+            ]);
+
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => 'error',
+                'message' => $e->getMessage()
+            ], 400);
+        }
+    }
+
+    public function updateActive()
+    {
+        $ids = AltaID::all();
+        foreach($ids as $id) {
+            Product::where(['sku' => 'ALTA-'.$id['product_id']])->update(['active' => 1, 'show' => 1]);
+            Log::warning('ALTA-'.$id['product_id'].' Updated');
+        }
     }
 }
