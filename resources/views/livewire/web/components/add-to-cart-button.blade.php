007c64d3e6ace2d3704764c0c8dd14fb455c7ff0
tob1tbs <chikhladze.mt@gmail.com>
2026-02-18 21:20:15 +0400

    Update add-to-cart-button.blade.php

--- resources/views/livewire/web/components/add-to-cart-button.blade.php
+++ resources/views/livewire/web/components/add-to-cart-button.blade.php
@@ -1,22 +1,14 @@
 <div>
     <button type="button"
             wire:click="addProduct"
-            onclick="pixelAddToCart(
-                {{ $productId }},
-                {{ $productPrice }},
-                @js($productTitle),
-                @js($cartEventId),
-            )"
             class="product-card-button btn btn-icon btn-primary animate-slide-end ms-2"
             aria-label="კალათაში დამატება"
-            data-url="{{ url()->current() }}"
             wire:loading.attr="disabled">
         <i class="ci-shopping-cart fs-base animate-target"></i>
     </button>
 </div>
 <script>
     Livewire.on('fb-add-to-cart', (data) => {
-        console.log(data);
         fbq('track', 'AddToCart', {
             content_ids: [String(data.id)],
             content_type: 'product',
@@ -25,7 +17,7 @@
             currency: 'GEL',
             contents: [{ id: String(data.id), quantity: data.quantity }]
         }, {
-            eventID: data.eventId
+            eventID: data.eventId // ✅ dispatch-იდან მოდის სწორი eventId
         });
     });
 </script>
