2ba0a1a3ca448adcd08991f4e32a7611f0d01cad
tob1tbs <chikhladze.mt@gmail.com>
2026-02-18 18:26:52 +0400

    UPDT

--- app/Livewire/Web/Main/Index.php
+++ app/Livewire/Web/Main/Index.php
@@ -22,9 +22,6 @@ class Index extends Component
     {
         $this->eventId = view()->shared('fb_event_id', 'pv_' . time() . '_' . Str::random(6));
         app(FacebookPixelService::class)->trackPageViewWithTest('TEST61083',[], $this->eventId);
-        Log::info('🔍 Index::mount() called111111', [
-            'event_id' => $this->eventId,
-        ]);
     }
 
     #[Computed]
