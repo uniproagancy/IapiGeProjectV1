8d5aa5e9cf9b53c349f8e38b9e84aab73cdce741
tob1tbs <chikhladze.mt@gmail.com>
2026-02-19 14:00:36 +0400

    Update CitrusController.php

--- app/Http/Controllers/ApiControllers/CitrusController.php
+++ app/Http/Controllers/ApiControllers/CitrusController.php
@@ -39,8 +39,7 @@ class CitrusController extends Controller
         $service = new CitrusProduct();
         $productData = $service->getProduct($request->product_url);
 
-        $check_product = Product::where('supplier_product_id', $productData['id'])->first();
-        dd($check_product);
+        $check_product = Product::where('supplier_product_id', $productData['id'])->where('supplier_id', $request->supplier_id)->first();
         if($productData['stock'] > 0 && $productData['stock_status'] === 1) {
             $quantity = $productData['stock'];
             $in_stock = $productData['stock_status'];
