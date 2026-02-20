0303cf8921fa1c6201a7e02a996e803f07dc9318
tob1tbs <chikhladze.mt@gmail.com>
2026-02-20 13:17:30 +0400

    Update Index.php

--- app/Livewire/Web/Promotions/Index.php
+++ app/Livewire/Web/Promotions/Index.php
@@ -8,6 +8,6 @@ class Index extends Component
 {
     public function render()
     {
-        return view('livewire.web.promotions.index');
+        return view('livewire.web.promotions.index')->layout('livewire.web.layout');
     }
 }
