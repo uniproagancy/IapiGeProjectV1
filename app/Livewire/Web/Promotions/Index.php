c0299ed5f26178fbace9ed87ecc84711598179c8
tob1tbs <chikhladze.mt@gmail.com>
2026-02-20 14:12:43 +0400

    UPDT

--- app/Livewire/Web/Promotions/Index.php
+++ app/Livewire/Web/Promotions/Index.php
@@ -9,13 +9,21 @@ use Livewire\Component;
 class Index extends Component
 {
 
+    public $sliderImg;
+
     protected $promotionSlug;
+    protected $promotionTitle;
     public function mount($promotionSlug) {
         $promotionPage = Page::where('slug', $promotionSlug)->firstOrFail();
         $promotionProducts = Product::where('promotion_id', $promotionPage->id)->get();
+        $this->sliderImg = $promotionPage->image;
+        $this->promotionTitle = $promotionPage->title;
     }
     public function render()
     {
-        return view('livewire.web.promotions.index')->layout('livewire.web.layout');
+        return view('livewire.web.promotions.index', [
+            'sliderImg' => $this->sliderImg ?? null,
+            'promotionTitle' => $this->promotionTitle ?? null,
+        ])->layout('livewire.web.layout');
     }
 }
--- resources/views/livewire/web/promotions/index.blade.php
+++ resources/views/livewire/web/promotions/index.blade.php
@@ -1,3 +1,25 @@
 <div>
-    {{-- Because she competes with no one, no one can compete with her. --}}
+    <main class="content-wrapper">
+        <nav class="container pt-3 my-3 my-md-4" aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="/">მთავარი</a></li>
+                <li class="breadcrumb-item active" aria-current="page">კატალოგი</li>
+            </ol>
+        </nav>
+        <h1 class="h3 container mb-4 font-neue">
+            {{ $promotionTitle }}
+        </h1>
+        <section class="container mb-4">
+            <div class="row">
+                <div class="col-lg-9">
+                    <div class="d-md-flex align-items-start">
+                        <div class="h6 fs-sm fw-normal text-nowrap translate-middle-y mt-3 mb-0 me-4 font-neue">
+                            ნაპოვნია <span class="fw-semibold">{{ $this->products->total() }}</span> პროდუქტი
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <hr class="d-lg-none my-3">
+        </section>
+    </main>
 </div>
