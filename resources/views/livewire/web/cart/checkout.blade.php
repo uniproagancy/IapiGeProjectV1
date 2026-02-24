3458f8357346a0e7d74a401f1aefe95184da6d7f
tob1tbs <chikhladze.mt@gmail.com>
2026-02-25 03:11:51 +0400

    Update checkout.blade.php

--- resources/views/livewire/web/cart/checkout.blade.php
+++ resources/views/livewire/web/cart/checkout.blade.php
@@ -101,7 +101,7 @@
             value: {{ $total ?? 0 }},
             currency: 'GEL',
             content_type: 'product',
-            num_items: {{ $items_count }}
-        }, { eventID: '{{ $event_id }}' });
+            num_items: {{ $orderItems ?? 0 }}
+        }, { eventID: '{{ $checkout_event_id }}' });
     </script>
 @endsection
