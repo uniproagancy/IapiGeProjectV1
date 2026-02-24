f17a7a9e51a006e64ae23186222ca427d8dc5f97
tob1tbs <chikhladze.mt@gmail.com>
2026-02-24 16:56:19 +0400

    Update BOGInstallment.php

--- app/Services/Payments/BOGInstallment.php
+++ app/Services/Payments/BOGInstallment.php
@@ -146,7 +146,7 @@ class BOGInstallment
     public function installmentCallback($installment_order_id)
     {
         $callback = Http::withToken($this->getToken())
-            ->post('https://installment.bog.ge/v1/installment/checkout/'.$installment_order_id)
+            ->get('https://installment.bog.ge/v1/installment/checkout/'.$installment_order_id)
             ->json();
         dd($callback);
     }
