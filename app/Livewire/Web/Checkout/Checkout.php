e25145f8d89b8e2b859babd26ff207840b7b7959
tob1tbs <chikhladze.mt@gmail.com>
2026-02-25 02:59:45 +0400

    Update Checkout.php

--- app/Livewire/Web/Checkout/Checkout.php
+++ app/Livewire/Web/Checkout/Checkout.php
@@ -167,7 +167,7 @@ class Checkout extends Component
                     'quantity' => $item['quantity'],
                 ];
             }
-            dd($items, count($items));
+            Log::warning(count($items));
             app(FacebookPixelService::class)->trackCheckout(
                 value: $this->total,
                 currency: 'GEL',
