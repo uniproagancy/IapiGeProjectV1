b65bb452ec1e3b7ca0b626f2189acef8dd1d07be
tob1tbs <chikhladze.mt@gmail.com>
2026-02-24 17:11:21 +0400

    Update BOGInstallmentController.php

--- app/Http/Controllers/ApiControllers/BOGInstallmentController.php
+++ app/Http/Controllers/ApiControllers/BOGInstallmentController.php
@@ -41,12 +41,12 @@ class BOGInstallmentController extends Controller
             foreach($orders as $order) {
                 if(!empty($order->transaction->payment_order_id)) {
                     $orderData = (new \App\Services\Payments\BOGInstallment)->installmentCallback($order->transaction->payment_order_id);
-                    switch ($orderData->installment_status) {
+                    switch ($orderData['installment_status']) {
                         case 'success':
-                            Order::find($order->shop_order_id)->update(['payment_status_id' => 2]);
+                            Order::find($order['shop_order_id'])->update(['payment_status_id' => 2]);
                         break;
                         default:
-                            Order::find($order->shop_order_id)->update(['payment_status_id' => 3]);
+                            Order::find($order['shop_order_id'])->update(['payment_status_id' => 3]);
                     }
                 }
             }
