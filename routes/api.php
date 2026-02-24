ea6d4b69db48d69c4216b335ac2bee74e344e7f2
tob1tbs <chikhladze.mt@gmail.com>
2026-02-24 16:57:22 +0400

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -31,7 +31,7 @@ Route::prefix('json')->group(function () {
 
 Route::prefix('bog')->group(function () {
     Route::post('/payment/callback', '\App\Http\Controllers\ApiControllers\BOGPaymentController@callback')->name('bog.payment-callback');
-    Route::get('/installment/callback', '\App\Http\Controllers\ApiControllers\BOGInstallmentController@callback')->name('bog.installment-callback');
+    Route::get('/installment/callback', '\App\Http\Controllers\ApiControllers\BOGInstallmentController@installmentCheck')->name('bog.installment-callback');
     Route::post('/installment/{order_id}', '\App\Http\Controllers\ApiControllers\BOGInstallmentController@createInstallment')->name('bog.create-installment-order');
     Route::post('/part-installment/{order_id}', '\App\Http\Controllers\ApiControllers\BOGInstallmentController@createPartInstallment')->name('bog.create-part-installment-order');
 });
