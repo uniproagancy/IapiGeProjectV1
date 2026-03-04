f809f33fa0fa5b21a836c3717cb208d18e35091d
uniproagancy <developer@unipro.ge>
2026-03-04 17:14:42 +0400

    UPDT

--- app/Jobs/ZoommerProductJob.php
+++ app/Jobs/ZoommerProductJob.php
@@ -159,21 +159,14 @@ class ZoommerProductJob implements ShouldQueue
                 'discount_price' => $discountPrice,
                 'discount_percent' => $productData['discountPercent'] ?? 0,
             ]);
-//            Storage::disk('public')->delete($product->main_image);
-            $product->update([
+            $updateData = [
                 'quantity' => $hasStock ? 5 : 0,
                 'in_stock' => $hasStock ? 1 : 0,
-                'show' => $hasStock ? 1 : 0,
-//                'main_image' => 1,
-            ]);
-//            $productGallery = ProductImage::where('product_id', $product->id)->get();
-//            foreach ($productGallery as $image) {
-//                Storage::disk('public')->delete($image->path);
-//            }
-//            ProductImage::where('product_id', $product->id)->forceDelete();
-//            $this->downloadImages($product, $productData);
-//            Log::info("✏️  Updated product: {$product->id}");
-
+            ];
+            if (!in_array($product->category_id, [18, 112, 125])) {
+                $updateData['show'] = $hasStock ? 1 : 0;
+            }
+            $product->update($updateData);
         } catch (Exception $e) {
             Log::error("Error updating product {$productData['id']}: {$e->getMessage()}");
             throw $e;
