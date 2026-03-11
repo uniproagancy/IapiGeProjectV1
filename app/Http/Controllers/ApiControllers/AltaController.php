eed40cc27a1a01885400e68db13f1aa93b2dde7d
uniproagancy <developer@unipro.ge>
2026-03-11 16:26:51 +0400

    Update AltaController.php

--- app/Http/Controllers/ApiControllers/AltaController.php
+++ app/Http/Controllers/ApiControllers/AltaController.php
@@ -48,7 +48,7 @@ class AltaController extends Controller
                     'show' => $show,
                     'quantity' => $this->parseQtyText($item->qty_text)['quantity'],
                 ]);
-                Log::info('ALTA-'.$item->item.' Quantity Updated Show status:'.$show);
+                Log::info('ALTA-'.$item->item.' Quantity Updated '.$this->parseQtyText($item->qty_text)['quantity'].' Show status:'.$show);
             }
         } catch (Exception $e) {
             throw new Exception('ფასების მიღება ვერ მოხერხდა: ' . $e->getMessage());
