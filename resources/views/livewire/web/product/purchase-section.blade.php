3f8e4af10b80c13795b2ea43393d37437ea468fe
uniproagancy <developer@unipro.ge>
2026-03-21 16:28:37 +0400

    UPDT

--- resources/views/livewire/web/product/purchase-section.blade.php
+++ resources/views/livewire/web/product/purchase-section.blade.php
@@ -62,41 +62,41 @@
     @endforeach
     @include('livewire.web.product.delivery-info')
     @if(Route::is('web.products.view'))
-{{--        <nav class="navbar navbar-expand navbar-dark bg-dark fixed-bottom d-lg-none bottom-nav-top"--}}
-{{--             style="padding: 0.75rem 0; border-top: 1px solid #333; z-index: 1000;">--}}
-{{--            <div class="container px-2">--}}
-{{--                <div class="d-flex w-100 justify-content-between align-items-center"  style="margin-bottom: 20px;">--}}
-{{--                    <div>--}}
-{{--                        <div class="d-flex flex-wrap align-items-center">--}}
-{{--                            @if(!empty($product->price->discount_price))--}}
-{{--                                <div class="h4 lh-1 mb-0" style="color: #fff;">--}}
-{{--                                    {{ number_format($product->price->discount_price, 2) }} ₾--}}
-{{--                                    <del class="text-body-tertiary fs-sm fw-normal" style="color: #252525 !important;">--}}
-{{--                                        {{ number_format($product->price->regular_price, 2) }}--}}
-{{--                                    </del>--}}
-{{--                                </div>--}}
-{{--                            @else--}}
-{{--                                <div class="h4 lh-1 mb-0" style="color: #fff;">--}}
-{{--                                    {{ number_format($product->price->regular_price, 2) }} ₾--}}
-{{--                                </div>--}}
-{{--                            @endif--}}
-{{--                            @if(!empty($product->price->discount_price) && $product->price->discount_price > 100 OR $product->price->regular_price > 100)--}}
-{{--                                <div class="d-flex flex-wrap flex-sm-nowrap flex-md-wrap flex-lg-nowrap gap-3 gap-lg-1 gap-xl-1" style="margin-bottom: 6px">--}}
-{{--                                    <span class="badge text-bg-success">თვეში @if(!empty($product->price->discount_price))--}}
-{{--                                        {{ number_format($product->price->discount_price / 24) }} @else {{ number_format($product->price->regular_price / 24) }} @endif ₾ -დან--}}
-{{--                                    </span>--}}
-{{--                                </div>--}}
-{{--                            @endif--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
-{{--                    <div class="d-flex justify-content-end">--}}
-{{--                        <livewire:web.components.add-to-cart-button-white :productId="$product->id"/>--}}
-{{--                        <a href="{{ route('web.checkout.index', ['product_id' => $product->id]) }}" class="btn btn-lg btn-dark w-100 animate-slide-end font-neue">--}}
-{{--                            ყიდვა / განვადება--}}
-{{--                        </a>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--        </nav>--}}
+        <nav class="navbar navbar-expand navbar-dark bg-dark fixed-bottom d-lg-none bottom-nav-top"
+             style="padding: 0.75rem 0; border-top: 1px solid #333; z-index: 1000;">
+            <div class="container px-2">
+                <div class="d-flex w-100 justify-content-between align-items-center"  style="margin-bottom: 20px;">
+                    <div>
+                        <div class="d-flex flex-wrap align-items-center">
+                            @if(!empty($product->price->discount_price))
+                                <div class="h4 lh-1 mb-0" style="color: #fff;">
+                                    {{ number_format($product->price->discount_price, 2) }} ₾
+                                    <del class="text-body-tertiary fs-sm fw-normal" style="color: #252525 !important;">
+                                        {{ number_format($product->price->regular_price, 2) }}
+                                    </del>
+                                </div>
+                            @else
+                                <div class="h4 lh-1 mb-0" style="color: #fff;">
+                                    {{ number_format($product->price->regular_price, 2) }} ₾
+                                </div>
+                            @endif
+                            @if(!empty($product->price->discount_price) && $product->price->discount_price > 100 OR $product->price->regular_price > 100)
+                                <div class="d-flex flex-wrap flex-sm-nowrap flex-md-wrap flex-lg-nowrap gap-3 gap-lg-1 gap-xl-1" style="margin-bottom: 6px">
+                                    <span class="badge text-bg-success">თვეში @if(!empty($product->price->discount_price))
+                                        {{ number_format($product->price->discount_price / 24) }} @else {{ number_format($product->price->regular_price / 24) }} @endif ₾ -დან
+                                    </span>
+                                </div>
+                            @endif
+                        </div>
+                    </div>
+                    <div class="d-flex justify-content-end">
+                        <livewire:web.components.add-to-cart-button-white :productId="$product->id"/>
+                        <a href="{{ route('web.checkout.index', ['product_id' => $product->id]) }}" class="btn btn-lg btn-dark w-100 animate-slide-end font-neue">
+                            ყიდვა / განვადება
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </nav>
     @endif
 </div>
