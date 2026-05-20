93f244f4ccbc0d0b3b6f4e73d9613be7e202736d
uniproagancy <developer@unipro.ge>
2026-05-20 14:15:29 +0400

    Update index.blade.php

--- resources/views/livewire/web/product/index.blade.php
+++ resources/views/livewire/web/product/index.blade.php
@@ -1,6 +1,6 @@
 @section('seo')
     <title>
-       {{  $currentCategory?->translation('ka')->title ?? 'პროდუქციის ჩამონათვალი' }}
+       {{  $currentCategory?->translation('ka')->title.' - IAPI.GE' ?? 'პროდუქციის ჩამონათვალი - IAPI.GE' }}
     </title>
     <meta name="description" content="Iapi.Ge">
     <meta name="keywords" content="Iapi.ge, იაფი,ჯი, იაფი, მაღაზია, ტექნიკა, ტელეფონები, სმარტფონები, კომპიუტერული ტექნიკა, მაცივრები, გათბობის სისტემები, Phones, Tech, PC, Refrigerators, Air cond,">
