eb246f52415bc707aff91958a8c90e53d1f4c4ac
uniproagancy <developer@unipro.ge>
2026-05-20 13:58:27 +0400

    UPDT

--- resources/views/livewire/web/layout.blade.php
+++ resources/views/livewire/web/layout.blade.php
@@ -2,7 +2,7 @@
 <html lang="en" data-bs-theme="light" data-pwa="true">
 <head>
     @section('seo')
-        <title>Iapi.Ge - იაფი მაღაზია</title>
+        <title>IAPI.GE - იაფი მაღაზია</title>
         <meta name="description" content="Iapi.Ge">
         <meta name="keywords"
               content="Iapi.ge, იაფი,ჯი, იაფი, მაღაზია, ტექნიკა, ტელეფონები, სმარტფონები, კომპიუტერული ტექნიკა, მაცივრები, გათბობის სისტემები, Phones, Tech, PC, Refrigerators, Air cond,">
