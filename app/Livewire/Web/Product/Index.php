6bf5cca98ce8950c84d177acd1f09d9adf605dc5
uniproagancy <developer@unipro.ge>
2026-05-27 16:45:04 +0400

    Update Index.php

--- app/Livewire/Web/Product/Index.php
+++ app/Livewire/Web/Product/Index.php
@@ -272,6 +272,7 @@ class Index extends Component
     public function products()
     {
         $result = $this->buildProductQuery()
+            ->orderBy('db_products.id', 'DESC')
             ->paginate($this->perPage);
         $this->isLoading = false;
         return $result;
