3935480a05dae7898a7224b27fc25d1f5d01f4b3
tob1tbs <chikhladze.mt@gmail.com>
2026-06-17 15:03:11 +0400

    123

--- resources/views/livewire/web/partials/bottom-menu.blade.php
+++ resources/views/livewire/web/partials/bottom-menu.blade.php
@@ -11,12 +11,12 @@
             <button type="button"
                     class="nav-link-custom text-center text-white-50 border-0 bg-transparent"
                     data-bs-toggle="offcanvas"
-                    data-bs-target="#categoryOffcanvas"
-                    aria-controls="categoryOffcanvas"
+                    data-bs-target="#navbarNav"
+                    aria-controls="navbarNav"
                     aria-label="Toggle navigation"
-                    title="კატეგორიები">
+                    title="ნავიგაცია">
                 <i class="ci-menu fs-5 mb-1"></i>
-                <div class="nav-label font-neue">კატეგორიები</div>
+                <div class="nav-label font-neue">ნავიგაცია</div>
             </button>
             <button type="button"
                     class="nav-link-custom text-center text-white-50 border-0 bg-transparent position-relative"
--- resources/views/livewire/web/partials/header/header.blade.php
+++ resources/views/livewire/web/partials/header/header.blade.php
@@ -7,8 +7,8 @@
                 <button type="button"
                         class="navbar-toggler me-4 me-lg-0"
                         data-bs-toggle="offcanvas"
-                        data-bs-target="#navbarNav"
-                        aria-controls="navbarNav"
+                        data-bs-target="#categoryOffcanvas"
+                        aria-controls="categoryOffcanvas"
                         aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
