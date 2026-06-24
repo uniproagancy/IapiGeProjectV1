a7f345def8b9ff386c94d902882773c0189d605e
tob1tbs <chikhladze.mt@gmail.com>
2026-06-24 17:05:40 +0400

    123

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,9 @@ use App\Models\AltaID;
 Route::get('/facebook-feed', '\App\Http\Controllers\FacebookFeedController@getFeed')->middleware('doNotCacheResponse')->name('facebook.get-feed');
 Route::get('/facebook-discount-feed', '\App\Http\Controllers\FacebookFeedController@getDiscountFeed')->middleware('doNotCacheResponse')->name('facebook.get-discount-feed');
 
-Route::post('/cart/add', '\App\Http\Controllers\CartController@add');
-Route::post('/wishlist/toggle', '\App\Http\Controllers\WishlistController@toggle');
-Route::post('/wishlist/check', '\App\Http\Controllers\WishlistController@check');
+Route::post('/cart/add', '\App\Http\Controllers\Web\CartController@add');
+Route::post('/wishlist/toggle', '\App\Http\Controllers\Web\WishlistController@toggle');
+Route::post('/wishlist/check', '\App\Http\Controllers\Web\WishlistController@check');
 
 Route::group(['prefix' => LaravelLocalization::setLocale()], function () {
 
