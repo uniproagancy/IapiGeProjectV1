06fb0f8eb5b75cf8abbc526eeeb82cbf2e1a5506
tob1tbs <chikhladze.mt@gmail.com>
2026-06-25 14:28:40 +0400

    Update category-section.blade.php

--- resources/views/livewire/web/partials/category-section.blade.php
+++ resources/views/livewire/web/partials/category-section.blade.php
@@ -13,7 +13,7 @@
             </a>
         </div>
     </div>
-    <div class="position-relative">
+    <div class="position-relative py-2">
         <div class="product-swiper overflow-hidden" data-section="{{ $category->id }}">
             <div class="swiper-wrapper">
                 @foreach($products as $product)
