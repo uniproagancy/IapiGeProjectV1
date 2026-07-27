04a1432998826a5b1eedd1dd945da61e6d1a10f4
tob1tbs <chikhladze.mt@gmail.com>
2026-07-27 11:58:24 +0400

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -44,7 +44,7 @@ Route::prefix('bog')->group(function () {
     Route::post('/payment/callback', '\App\Http\Controllers\ApiControllers\BOGPaymentController@callback')->name('bog.payment-callback');
     Route::get('/installment/callback', '\App\Http\Controllers\ApiControllers\BOGInstallmentController@installmentCheck')->name('bog.installment-callback');
     Route::post('/installment/{order_id}', '\App\Http\Controllers\ApiControllers\BOGInstallmentController@createInstallment')->name('bog.create-installment-order');
-    Route::post('/installment/checkmanualy/{order_id}', '\App\Http\Controllers\ApiControllers\BOGInstallmentController@checkManualy')->name('bog.check-manualy');
+    Route::get('/installment/checkmanualy/{order_id}', '\App\Http\Controllers\ApiControllers\BOGInstallmentController@checkManualy')->name('bog.check-manualy');
     Route::post('/part-installment/{order_id}', '\App\Http\Controllers\ApiControllers\BOGInstallmentController@createPartInstallment')->name('bog.create-part-installment-order');
     Route::post('/part-installment/{order_id}', '\App\Http\Controllers\ApiControllers\BOGInstallmentController@createPartInstallment')->name('bog.create-part-installment-order');
     Route::get('/refund', '\App\Http\Controllers\ApiControllers\BogRefundController@refund')->name('bog.refund');
