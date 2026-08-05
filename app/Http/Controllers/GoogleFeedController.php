d6075e003609cb2e3f393cd13998f06957265996
tob1tbs <chikhladze.mt@gmail.com>
2026-08-05 11:21:20 +0400

    Update GoogleFeedController.php

--- app/Http/Controllers/GoogleFeedController.php
+++ app/Http/Controllers/GoogleFeedController.php
@@ -127,7 +127,7 @@ class GoogleFeedController extends Controller
                         $productPrice = $salePrice ?? $regularPrice;
 
                         $item = [
-                            'g:id'                     => (string) $product->id,
+                            'id'                     => (string) $product->id,
                             'title'                  => $translation->title,
                             'description'            => $description,
                             'link'                   => route('web.products.view', $translation->slug),
