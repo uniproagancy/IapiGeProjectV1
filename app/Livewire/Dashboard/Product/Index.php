0cf2492dc213c833c108f24838d27ed9fc4a6820
tob1tbs <chikhladze.mt@gmail.com>
2026-08-11 13:49:26 +0400

    Update Index.php

--- app/Livewire/Dashboard/Product/Index.php
+++ app/Livewire/Dashboard/Product/Index.php
@@ -683,9 +683,10 @@ class Index extends Component
             foreach ($sheet->getRowIterator() as $row) {
 
                 $barCode = trim((string) $sheet->getCell('A' . $row->getRowIndex())->getValue());
-                Product::where('sku', 'LIKE', 'ELITE-'.$barCode)->update([
-                    'show' => 1
-                ]);
+                Log::info($barCode);
+//                Product::where('sku', 'LIKE', 'ELITE-'.$barCode)->update([
+//                    'show' => 1
+//                ]);
                 if (!$barCode) { $skipped++; continue; }
 
                 if (\App\Models\EliteProduct::where('bar_code', $barCode)->exists()) {
